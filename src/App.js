6a2bcb2c70ded4487d891aacc15f262b3b11e46b
GregSherman <greg.sherman@mail.utoronto.ca>
2022-02-22 21:31:38 -0500

    added resume file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             <h1 className="bigName">greg sherman</h1>
             <p className="subText">under construction</p>
             <a href="https://github.com/GregSherman" className="link">GitHub  </a>
-            <a href="resources/Greg Resume.pdf" className="link">  Resume  </a>
+            <a href="resources/gregresume.pdf" className="link">  Resume  </a>
             <a href="https://www.linkedin.com/in/greg-sherman-aa20101b5/" className="link">  LinkedIn  </a>
             <a href="mailto: greg.sherman@mail.utoronto.ca" className="link">  Email  </a>
         </div>
