3237fed067ff893817ab0d648772d90398d7f00e
Greg Sherman <63402349+GregSherman@users.noreply.github.com>
2022-02-22 21:35:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-}
