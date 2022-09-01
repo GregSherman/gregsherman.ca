b01216b36900449edbe30092b27c4d9eb5af82ba
GregSherman <greg.sherman@mail.utoronto.ca>
2022-08-31 23:15:39 -0300

    fixed resume link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             <h1 className="bigName">greg sherman</h1>
             <p className="subText">software engineer</p>
             <a href="https://github.com/GregSherman" target="_blank" rel="noopener noreferrer" className="link">GitHub  </a>
-            <a href="https://github.com/GregSherman/gregsherman.ca/blob/e77cd118151e53316ced36ed4513893ce022b632/public/gregresume.pdf" className="link">  Resume  </a>
+            <a href="https://github.com/GregSherman/gregsherman.ca/raw/main/public/gregresume.pdf" className="link">  Resume  </a>
             <a href="https://www.linkedin.com/in/gregsherman-/" target="_blank" rel="noopener noreferrer" className="link">  LinkedIn  </a>
             <a href="mailto: swe@gregsherman.ca" className="link">  Email  </a>
         </div>
