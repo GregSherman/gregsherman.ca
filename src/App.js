0d3baa256b686bdbeef645fee65f0f8a226219e5
GregSherman <greg.sherman@mail.utoronto.ca>
2022-08-31 23:04:13 -0300

    updated resume and email, removed under construction

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
         <ParticleBG/>
         <div className="mainPage">
             <h1 className="bigName">greg sherman</h1>
-            <p className="subText">under construction</p>
+            <p className="subText">software engineer</p>
             <a href="https://github.com/GregSherman" target="_blank" rel="noopener noreferrer" className="link">GitHub  </a>
             <a href="https://github.com/GregSherman/gregsherman.ca/blob/e77cd118151e53316ced36ed4513893ce022b632/public/gregresume.pdf" className="link">  Resume  </a>
             <a href="https://www.linkedin.com/in/gregsherman-/" target="_blank" rel="noopener noreferrer" className="link">  LinkedIn  </a>
-            <a href="mailto: greg.sherman@mail.utoronto.ca" className="link">  Email  </a>
+            <a href="mailto: swe@gregsherman.ca" className="link">  Email  </a>
         </div>
         </div>
     );
